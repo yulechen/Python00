58c84a76f3134a8386743eb032bdbc6d999b28b2
gc_chenqiu <gc_chenqiu@huoyunren.com>
2017-06-06 15:05:17 +0800

    添加 网络编程接口说明
    Signed-off-by: gc_chenqiu <gc_chenqiu@huoyunren.com>

---
+++ PyTest/src/tool/Data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Created on 2017年6月6日
+
+@author: Huoyunren
+'''
+print chr(65)
+print ord('a')
+print unichr(12345)
+# print chr(12345) （0-255）
+print ord(u'\u2345')
+print repr('ss')  # 讲对象显示为字符
