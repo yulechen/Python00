bb93542a8fb9a0c3f70193559b0e4624938e3ad1
gc_chenqiu <gc_chenqiu@huoyunren.com>
2017-06-09 14:59:09 +0800

    添加参数解析
    Signed-off-by: gc_chenqiu <gc_chenqiu@huoyunren.com>

--- PyTest/src/pybase/argparse/Test01.py
+++ PyTest/src/pybase/argparse/Test01.py
@@ -5,7 +5,14 @@ Created on 2017年6月9日
 
 @author: Huoyunren
 '''
+
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.parse_args()
+parser = argparse.ArgumentParser(description='Short sample app')
+
+parser.add_argument('-a', action="store_true", default=False)
+parser.add_argument('-b', action="store", dest="b")
+parser.add_argument('-c', action="store", dest="c", type=int)
+# -a -bval -c 3
+args = parser.parse_args(['-a', '-bval', '-c', '3'])
+print args.b, args.c
